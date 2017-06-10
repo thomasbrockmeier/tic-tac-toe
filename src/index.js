85683818c919c6bb91c7242f070b447cb7eec205
Thomas Brockmeier <t.brockmeier@youngcapital.nl>
2017-06-10 11:52:11 +0200

    clean up a but

--- src/index.js
+++ src/index.js
@@ -60,19 +60,19 @@ class Board extends React.Component {
       <div>
         <div className="status">{ status }</div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          { this.renderSquare(0) }
+          { this.renderSquare(1) }
+          { this.renderSquare(2) }
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          { this.renderSquare(3) }
+          { this.renderSquare(4) }
+          { this.renderSquare(5) }
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          { this.renderSquare(6) }
+          { this.renderSquare(7) }
+          { this.renderSquare(8) }
         </div>
       </div>
     );
@@ -86,10 +86,6 @@ class Game extends React.Component {
         <div className="game-board">
           <Board />
         </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
       </div>
     );
   }
@@ -101,6 +97,8 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+// ========================================
 
 function calculateWinner(squares) {
   const lines = [
