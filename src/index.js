e127ef18aa5f155a2bd76434aab8fd865e32103c
Thomas Brockmeier <t.brockmeier@youngcapital.nl>
2017-06-10 10:58:58 +0200

    add deps

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-ass Square extends React.Component {
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Square extends React.Component {
   render() {
     return (
       <button className="square">
